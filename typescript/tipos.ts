d8fb50293875ff19b745b1bf9e3a584940fcb7f8
Marco Jardim <santarelle@gmail.com>
2017-09-19 22:33:32 -0400

    tipos com typescript

---
+++ typescript/tipos.ts
@@ -0,0 +1,9 @@
+let mensagem: string = "Seja bem vindo"
+let temporadasFriends: number = 10
+let estuadndoAngular: boolean = true
+
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+let listaDeFrutas2: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+
+let notasDasProvas: Array<number> = [7.5, 8, 9]
+let notasDasProvas2: number[] = [7.5, 8, 9]
