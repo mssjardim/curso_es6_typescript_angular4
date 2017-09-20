d76cf0bd3290a10121f4ae44055ccd5314118eca
Marco Jardim <santarelle@gmail.com>
2017-09-20 00:14:09 -0400

    adicionando classe pessoa

--- typescript/classesEobjetos.ts
+++ typescript/classesEobjetos.ts
@@ -39,5 +39,32 @@ class Concessionaria {
 }
 
 
-let concessionaria = new Concessionaria('Av. Paulista')
-console.log(concessionaria)
+class Pessoa {
+    private nome: string
+    private carroPreferido: string
+    private carro: any
+
+    constructor(nome: string, carroPreferido: string) {
+        this.nome = nome
+        this.carroPreferido = carroPreferido
+    }
+
+    public dizerNome(): string {
+        return this.nome
+    }
+
+    public dizerCarroPreferido(): string {
+        return this.carroPreferido
+    }
+
+    public comprarCarro(carro: any): void {
+        this.carro = carro
+    }
+
+    public dizerCarroQueTem(): any {
+        return this.carro
+    }
+}
+
+let pessoa = new Pessoa('Marco', 'Veloster')
+console.log(pessoa.dizerCarroPreferido())
