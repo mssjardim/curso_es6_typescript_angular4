e245b12a405a060ec8064e65388b03b322a23602
Marco Jardim <santarelle@gmail.com>
2017-09-20 00:03:04 -0400

    classe Concessionaria

--- typescript/classesEobjetos.ts
+++ typescript/classesEobjetos.ts
@@ -21,10 +21,23 @@ class Carro {
     }
 }
 
-let carroA = new Carro('Veloster', 3)
-console.log(carroA)
-carroA.acelerar()
-console.log(carroA)
-carroA.acelerar()
-carroA.acelerar()
-console.log(carroA)
+class Concessionaria {
+    private endereco: string
+    private listaDeCarros: any
+
+    constructor(endereco: string) {
+        this.endereco = endereco
+    }
+
+    public fornecerEndereco(): string {
+        return this.endereco
+    }
+
+    public mostrarListaDeCarros(): any {
+        return this.listaDeCarros
+    }
+}
+
+
+let concessionaria = new Concessionaria('Av. Paulista')
+console.log(concessionaria)
