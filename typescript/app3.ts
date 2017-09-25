dcdedf047cd9d448c80f714dea620135d6cefd2c
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-09-25 17:37:08 -0400

    herança (extends)

---
+++ typescript/app3.ts
@@ -0,0 +1,12 @@
+import Carro from './Carro'
+import Moto from './Moto'
+
+let carro = new Carro('veloster', 4)
+
+let moto = new Moto()
+moto.acelerar()
+moto.acelerar()
+
+console.log(moto);
+console.log(carro);
+
