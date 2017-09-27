e70db6356ce780eabec15cef1631a5c7444d37fc
Marco Jardim <santarelle@gmail.com>
2017-09-26 20:47:06 -0400

    interface básico

--- typescript/app3.ts
+++ typescript/app3.ts
@@ -1,13 +1,16 @@
 import Carro from './Carro'
 import Moto from './Moto'
+import Concessionaria from './Concessionaria'
 
 let carro = new Carro('veloster', 4)
 
 let moto = new Moto()
 moto.acelerar()
 moto.acelerar()
-console.log(moto);
-
 carro.acelerar()
-console.log(carro);
 
+let concessionaria = new Concessionaria('',[])
+
+console.log(moto);
+console.log(carro);
+console.log(concessionaria.fornecerHorariosDeFuncionamento());
