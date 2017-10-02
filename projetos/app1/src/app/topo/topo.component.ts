8f347bbf1e9c92b2fcbff823b4b1bcc8d12fc067
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-10-02 16:58:56 -0400

    template, string interpolation, property binding

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -2,12 +2,9 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-topo',
-    template: '<p>Template inline</p>',
     templateUrl: './topo.component.html',
-    styles: [` .exemplo { 
-        color: red 
-    } `],
     styleUrls: ['./topo.component.css']
-    
 })
-export class TopoComponent {}
+export class TopoComponent {
+    public titulo: string = 'Aprendendo Inglês'
+}
