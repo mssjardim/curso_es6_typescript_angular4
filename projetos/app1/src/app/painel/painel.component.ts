861c953a4acd63db9cb7319b5e81d49810c151a6
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-10-05 16:09:15 -0400

    One-way-binding vs Two-way-binding

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -12,10 +12,24 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
+  public resposta: string
 
-  constructor() { console.log(this.frases) }
+  public rodada:number = 0
+  public rodadaFrase:Frase
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+  }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value 
+    console.log( this.resposta );
+  }
+
+  public verificarResposta(): void {
+    console.log('Verificar resposta:', this.resposta);
+  }
 }
