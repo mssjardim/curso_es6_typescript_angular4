17ff951214da07cb33623a024270a0153fe20116
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-10-06 13:27:13 -0400

    finalizado app "Aprendendo Inglês"

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 import { Frase } from '../shared/frase.model'
 import { FRASES } from './frases-mock'
@@ -8,7 +8,7 @@ import { FRASES } from './frases-mock'
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
@@ -19,11 +19,18 @@ export class PainelComponent implements OnInit {
 
   public progresso: number = 0
 
+  public tentativas: number = 3
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
   constructor() {
     this.atualizaRodada()
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
   }
 
   public atualizaResposta(resposta: Event): void {
@@ -33,23 +40,30 @@ export class PainelComponent implements OnInit {
   public verificarResposta(): void {
 
     if (this.rodadaFrase.frasePtBr == this.resposta) {
-      alert('A tradução está correta')
 
       // trocar pergunta da rodada
       this.rodada++
 
       // progresso
       this.progresso = this.progresso + (100 / this.frases.length)
-      console.log(this.progresso);
+
+      // // verifica todos acertos
+      if (this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria')
+      }
 
       // atualiza a frase da rodada
       this.atualizaRodada()
 
     } else {
-      alert('A tradução está errada')
+      // diminuir a variavel tentativas
+      this.tentativas--
+
+      if (this.tentativas === -1) {
+        this.encerrarJogo.emit('derrota')
+      }
 
     }
-
   }
 
   public atualizaRodada(): void {
