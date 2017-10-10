ac241bc2f696e93e5055de264465bae278e94d58
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-10-10 13:44:19 -0400

    exibir ofertas por id

--- projetos/app2/src/app/oferta/oferta.component.ts
+++ projetos/app2/src/app/oferta/oferta.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { OfertasService } from '../ofertas.service'
+import { Oferta } from '../shared/oferta.model'
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public oferta: Oferta
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    // console.log(this.route.snapshot.params['id'])
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta
+      })
   }
-
 }
