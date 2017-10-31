8c39a99e6abb55ea9587c5f8ae5b760afb46b165
Marco Jardim <mjardim@digiboard.com.br>
2017-10-31 14:49:21 -0400

    adicionado componente ordem-compra

--- projetos/app2/src/app/app.module.ts
+++ projetos/app2/src/app/app.module.ts
@@ -16,7 +16,8 @@ import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 // pipe
-import { DescricaoReduzida } from './util/descricao-reduzida.pipe'
+import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component'
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { DescricaoReduzida } from './util/descricao-reduzida.pipe'
     OfertaComponent,
     ComoUsarComponent,
     OndeFicaComponent,
-    DescricaoReduzida
+    DescricaoReduzida,
+    OrdemCompraComponent
   ],
   imports: [
     BrowserModule,
