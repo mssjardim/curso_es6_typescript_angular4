133fd953ff554b0c142e8178bcaa2ff11339e973
Marco Jardim <mjardim@digiboard.com.br>
2017-11-08 16:42:45 -0400

    Reactive Forms

--- projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -11,9 +11,14 @@ import { NgForm } from '@angular/forms';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  @ViewChild('formulario') public formulario: NgForm
+  public idPedidoCompra: number
 
-  public idPedidoCompra: any
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(120) ]),
+    'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(20) ]),
+    'complemento': new FormControl(null),
+    'formaPagamento': new FormControl(null, [ Validators.required ])
+  })
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
@@ -22,17 +27,23 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
-    
-    let pedido: Pedido = new Pedido(
-      this.formulario.value.endereco,
-      this.formulario.value.numero,
-      this.formulario.value.complemento,
-      this.formulario.value.formaPagamento
-    )
-    this.ordemCompraService.efetivarCompra(pedido)
-    .subscribe((idPedido: number) => {      
-      this.idPedidoCompra = idPedido
-    })
+    if (this.formulario.status === 'INVALID') {
+      this.formulario.get('endereco').markAsTouched()
+      this.formulario.get('numero').markAsTouched()
+      this.formulario.get('complemento').markAsTouched()
+      this.formulario.get('formaPagamento').markAsTouched()
+    } else {
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      )
+      this.ordemCompraService.efetivarCompra(pedido)
+      .subscribe((idPedido: number) => {
+        this.idPedidoCompra = idPedido
+      })
+    }
   }
   
 }
