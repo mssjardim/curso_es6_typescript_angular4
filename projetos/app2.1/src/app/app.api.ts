792dad0a5f277e2b7997e5cc75ce06df6ff011b7
Marco Jardim <santarelle@gmail.com>
2017-11-09 22:12:12 -0400

    api rest free

--- projetos/app2.1/src/app/app.api.ts
+++ projetos/app2.1/src/app/app.api.ts
@@ -1,1 +1,2 @@
-export const URL_API = 'http://localhost:3000'
+// export const URL_API = 'http://localhost:3000'
+export const URL_API = 'https://my-json-server.typicode.com/mssjardim/curso_es6_typescript_angular4'
