2f09ee821d274d442f47546d20796c40a474219b
Marco Jardim <mjardim@digiboard.com.br>
2017-11-10 16:39:52 -0400

    adicionando firebase ao projeto

--- projetos/app3/src/app/app.component.ts
+++ projetos/app3/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    var config = {
+      apiKey: "AIzaSyBRU0DX8b0pgyBaJR73zuiU_g1nPcKKvqs",
+      authDomain: "jta-instagram-clone-de6f8.firebaseapp.com",
+      databaseURL: "https://jta-instagram-clone-de6f8.firebaseio.com",
+      projectId: "jta-instagram-clone-de6f8",
+      storageBucket: "jta-instagram-clone-de6f8.appspot.com",
+      messagingSenderId: "108214528294"
+    };
+    firebase.initializeApp(config);
+  }
+
 }
