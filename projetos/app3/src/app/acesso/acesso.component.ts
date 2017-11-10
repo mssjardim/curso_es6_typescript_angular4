c8b4ec631afead81f436b62f8d6da9f58eedd046
Marco Jardim <mjardim@digiboard.com.br>
2017-11-10 15:37:34 -0400

    keyframes

--- projetos/app3/src/app/acesso/acesso.component.ts
+++ projetos/app3/src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations'
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations'
 
 @Component({
   selector: 'app-acesso',
@@ -22,7 +22,17 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       transition('void => criado', [ 
         style({ opacity: 0, transform: 'translate(50px, 0px)' }),
-        animate('500ms 0s ease-in-out') // duracao, deplay e aceleracao
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)' }),
+
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)' }),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)' }),
+        ])) // duracao, deplay e aceleracao
       ])
     ])
   ]
@@ -41,6 +51,14 @@ export class AcessoComponent implements OnInit {
 
   public exibirPainel(event: string): void {
     this.cadastro = event === 'cadastro'
+  }
+
+  public inicioDaAnimacao(): void {
+    console.log('inicio');
+  }
+
+  public fimDaAnimacao(): void {
+    console.log('fim');
   }
 
 }
