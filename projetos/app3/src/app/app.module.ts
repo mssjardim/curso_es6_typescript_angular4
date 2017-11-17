478bfdd7c6f51fb28d7baa0e6a82a67f1eeb8d0b
Marco Jardim <santarelle@gmail.com>
2017-11-16 20:39:22 -0400

    salvando imagens no firebase

--- projetos/app3/src/app/app.module.ts
+++ projetos/app3/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ROUTES } from './app.routes'
 
 import { Autenticacao } from './autenticacao.service';
 import { AutenticacaoGuard } from './autenticacao-guard.service'
+import { Bd } from './bd.service'
+import { Progresso } from './progresso.service'
 
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -16,6 +18,7 @@ import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
 import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
+import { IncluirPublicacaoComponent } from './home/incluir-publicacao/incluir-publicacao.component';
 
 
 @NgModule({
@@ -27,6 +30,7 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
     CadastroComponent,
     HomeComponent,
     PublicacoesComponent,
+    IncluirPublicacaoComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,7 +38,7 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
     ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [ Autenticacao, AutenticacaoGuard ],
+  providers: [ Autenticacao, AutenticacaoGuard, Bd, Progresso ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ projetos/app3/src/app/bd.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core'
+
+import * as firebase from 'firebase'
+import { Progresso } from './progresso.service';
+
+@Injectable()
+export class Bd {
+
+    constructor(
+        private progresso: Progresso
+    ) { }
+
+    public publicar(publicacao: any): void {
+        
+        
+
+        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+            .push( { titulo: publicacao.titulo })
+            .then((resposta: any) => {
+
+                let nomeImagem = resposta.key
+
+                firebase.storage().ref()
+                .child(`imagens/${nomeImagem}`)
+                .put(publicacao.imagem)
+                .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                    (snapshot: any) => {
+                        this.progresso.status = 'andamento'
+                        this.progresso.estado = snapshot                    
+                    },
+                    (error) => {
+                        this.progresso.status = 'erro'
+                        console.log(error)
+                    },
+                    () => {
+                        this.progresso.status = 'concluido'                    
+                    }
+                )        
+
+            })
+
+        
+    }
+}
